b518437c24682f754a33477f1be4067d8fa22692
DVZ <polo1215@bk.ru>
2022-08-09 14:27:54 +0300

    Add Home Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите любое целое чиисло от 1 до 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = (number / 100);
+if (digit > 0)
+    Console.WriteLine($"Третья цифра введенного числа равна: {digit}");
+else
+    Console.WriteLine($"Вы ввели двузначное число, поэтому третьей цифры нет");
