7531bceaa0ef6d8e29c47e5552cf567eb5e2bfc6
DVZ <polo1215@bk.ru>
2022-08-09 14:05:26 +0300

    Улучшил ввывод результата

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("Введите любое трехзначное целое чиисло: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int digit = (number / 10) % 10;
-Console.WriteLine($"Второе число равно {digit}");
+Console.WriteLine($"Вторая цифра введенного числа равна: {digit}");
